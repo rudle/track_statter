e46a3acd6902e08bbbcbd5e623d23587cbd6a88f
Sean Sorrell <seansorrell@gmail.com>
2010-07-18 17:11:12 -0400

    hostsss

--- fantasy_baseball.rb
+++ fantasy_baseball.rb
@@ -9,10 +9,10 @@ require 'cgi'
 require 'sinatra'
 
 BASE_API_URL = "https://fantasysports.yahooapis.com/fantasy/v2/"
-HOST = 'http://quiet-light-99.heroku.com/'
 
 get '/' do
-host = HOST || 'http://localhost:4567/'
+	host = 'http://quiet-light-99.heroku.com/'
+	host = 'http://localhost:4567/'
 	$consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, :site => "https://api.login.yahoo.com/", :request_token_path => "/oauth/v2/get_request_token", :authorize_path => "/oauth/v2/request_auth", :oauth_callback => "#{host}callback", :access_token_path => "/oauth/v2/get_token")
 	$request_token = $consumer.get_request_token({:oauth_callback => "#{host}callback/"})
 	session[:request_token] = $request_token
@@ -30,8 +30,12 @@ get '*' do
 	$access_token = $request_token.get_access_token({:oauth_verifier => params["oauth_verifier"], :oauth_token => params["oauth_token"]}, :request_uri => BASE_API_URL  + query)
 
 
-	response = $access_token.get("/")
+	begin
+		response = $access_token.get("/")
+	rescue Exception
+		debugger
+	end
 
-	puts "hello"
+	
 end
 
